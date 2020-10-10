7e8a605cd196350a106b389bae2bec2f18af32e8
John <inkopro1@gmail.com>
2020-10-10 21:00:46 +0500

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+// let human = {
+//   name: 'John',
+//   surname: 'Galt',
+//   age: 26
+// };
+// console.log(human);
+
+// let arr = ['a', 'b', 'c'];
+// console.log(arr[0]);
+
+// let answer = +prompt("Вам есть 18 лет?", "ДА");
+// console.log(typeof(answer));
+// console.log('arr' + ' - object');
+// console.log(4 + + ' - object');
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log('2' === 2);
+
+let a = true,
+    b = false;
+console.log(a || b);
